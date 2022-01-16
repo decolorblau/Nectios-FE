2db58bb55a6bba938b4b2efa1bff398748ddf21b
Claudia Font <claudiafontmarques@gmail.com>
2022-01-17 00:03:43 +0100

    added nav bar and Product Card to Home

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,29 @@
 <template>
   <div>
-    <HelloWorld />
+    <NavBar />
+  </div>
+  <div>
+    <v-container class="grey lighten-5 mb-6">
+      <v-row :align="align" no-gutters style="height: 150px">
+        <v-col v-for="n in 3" :key="n">
+          <ProductCard />
+        </v-col>
+      </v-row>
+    </v-container>
   </div>
 </template>
 
 <script>
 import { mapActions, mapGetters, mapState } from "vuex";
-import HelloWorld from "../components/HelloWorld.vue";
+import NavBar from "../components/NavBar/NavBar.vue";
+import ProductCard from "../components/ProductCard/ProductCard.vue";
 
 export default {
   name: "Home",
 
   components: {
-    HelloWorld,
+    NavBar,
+    ProductCard,
   },
   computed: {
     ...mapState(["user"]),
