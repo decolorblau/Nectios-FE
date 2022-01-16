c14bbf1993988ab7609b1df396baf38ce523b037
Claudia Font <claudiafontmarques@gmail.com>
2022-01-16 22:17:55 +0100

    fixed object response destructuring - now is correct: data, status

--- src/store/actions.js
+++ src/store/actions.js
@@ -3,11 +3,12 @@ import jwtDecode from "jwt-decode";
 
 const actions = {
   async loginUser({ commit }, user) {
-    const { data } = await axios.post(`${process.env.VUE_APP_API_URL}/login`, user);
-    const { token } = data;
+    const { data, status } = await axios.post(`${process.env.VUE_APP_API_URL}/login`, user);
+    const token = data;
     const userInfo = jwtDecode(token);
     localStorage.setItem("token", JSON.stringify({ token }));
     commit("loadUser", userInfo);
+    return status;
   },
 };
 
