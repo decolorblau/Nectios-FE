ee3dc7032478397eef1d2d9edb847bc75fea4782
Claudia Font <claudiafontmarques@gmail.com>
2022-01-18 13:17:20 +0100

    added font-awsome library for icons

--- src/plugins/vuetify.js
+++ src/plugins/vuetify.js
@@ -1,10 +1,23 @@
 // Styles
-import "@mdi/font/css/materialdesignicons.css";
+/* import "@mdi/font/css/materialdesignicons.css";
+ */ // eslint-disable-next-line import/no-extraneous-dependencies
+import "@fortawesome/fontawesome-free/css/all.css";
 // eslint-disable-next-line import/no-unresolved
 import "vuetify/styles";
+import { aliases, fa } from "vuetify/lib/iconsets/fa";
+import { mdi } from "vuetify/lib/iconsets/mdi";
 
 // Vuetify
 import { createVuetify } from "vuetify";
 
-export default createVuetify();
+export default createVuetify({
+  icons: {
+    defaultSet: "fa",
+    aliases,
+    sets: {
+      fa,
+      mdi,
+    },
+  },
+});
 // https://vuetifyjs.com/en/introduction/why-vuetify/#feature-guides
