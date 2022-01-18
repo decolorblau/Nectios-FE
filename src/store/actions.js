9b85a491c1d5e1ec583b4f6391175afefe9bec9f
Claudia Font <claudiafontmarques@gmail.com>
2022-01-18 20:35:37 +0100

    added getProducts action

--- src/store/actions.js
+++ src/store/actions.js
@@ -12,8 +12,8 @@ const actions = {
   },
   checkToken({ commit }) {
     try {
-      const token = JSON.parse(localStorage.getItem("token") || "");
-      commit("loadUser", jwtDecode(token.token));
+      const { token } = JSON.parse(localStorage.getItem("token") || "");
+      commit("loadUser", jwtDecode(token));
     } catch {
       return "User not logged in";
     }
@@ -21,6 +21,30 @@ const actions = {
   async logoutUser({ commit }) {
     localStorage.removeItem("token");
     commit("logoutUser");
+  },
+  async getProducts({ commit, state }) {
+    try {
+      const { token } = JSON.parse(localStorage.getItem("token") || "");
+      const uri = `${process.env.VUE_APP_API_URL}/products?${state.user.clientKey}`;
+      const encoded = encodeURI(uri);
+      const { data } = await axios.get(
+        encoded,
+        {
+          headers: {
+            Authorization: `Bearer ${token}`,
+          },
+          // eslint-disable-next-line comma-dangle
+        }
+        // eslint-disable-next-line comma-dangle
+      );
+      const newData = data[0].data;
+      commit("getProducts", newData);
+    } catch {
+      return "Error";
+    }
+  },
+  getClientKey({ commit }, clientKey) {
+    commit("newClientKey", clientKey);
   },
 };
 
