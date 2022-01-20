b189b5e3c8d12c980af6ba33c76ff234172ed3a9
Claudia Font <claudiafontmarques@gmail.com>
2022-01-21 00:18:11 +0100

    Converted Name products to Uppercase

--- src/components/ProductCard/ProductCard.vue
+++ src/components/ProductCard/ProductCard.vue
@@ -2,7 +2,7 @@
   <v-card class="ma-1 mb-2" width="269.5">
     <v-img :src="product.product_photo" height="200px"></v-img>
     <div class="">
-      <v-card-title class="pb-1"> {{ product.name }} </v-card-title>
+      <v-card-title class="pb-1"> {{ product.name.toUpperCase() }} </v-card-title>
       <v-spacer></v-spacer>
       <v-card-subtitle class="pb-2">{{ product.price }} €</v-card-subtitle>
     </div>
