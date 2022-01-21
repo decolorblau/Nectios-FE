e3d389b744b2c20b778877ffbf5f9f1f4cb8ca6b
Claudia Font <claudiafontmarques@gmail.com>
2022-01-21 20:14:51 +0100

    added add product link into nav bar

--- src/components/NavBar/NavBar.vue
+++ src/components/NavBar/NavBar.vue
@@ -1,66 +1,78 @@
 <template>
-  <nav>
-    <v-app-bar density="comfortable" flat app>
-      <v-app-bar-title class="text-uppercase grey--text text-h4" to="/">
-        <router-link to="/" class="title dislay-1">
-          <span class="font-weight-light">E-</span>
-          <span>BLUE</span>
-        </router-link>
-      </v-app-bar-title>
-      <v-spacer></v-spacer>
-      <div class="hidden-sm-and-down">
-        <v-btn flat @click="redirectToHome()" color="none" class="mr-4 text-uppercase">
-          <span class="text-bar">Home</span>
-        </v-btn>
+  <v-app-bar density="comfortable" flat app>
+    <v-app-bar-title class="text-uppercase grey--text text-h4" to="/">
+      <router-link to="/" class="title display-1">
+        <span class="font-weight-light">BLUE</span>
+        <span class="world blue">WORLD</span>
+      </router-link>
+    </v-app-bar-title>
+    <v-spacer></v-spacer>
+    <v-list class="hidden-sm-and-down">
+      <v-btn flat @click="redirectToHome()" color="none" class="mr-4 text-uppercase">
+        <span class="text-bar">Home</span>
+      </v-btn>
 
-        <v-btn
-          to="/user"
-          @click="redirectToUser()"
-          flat
-          :ripple="false"
-          color="none"
-          class="mr-4 text-uppercase"
-        >
-          <span class="text-bar">User</span>
-        </v-btn>
+      <v-btn
+        to="/user"
+        @click="redirectToUser()"
+        flat
+        :ripple="false"
+        color="none"
+        class="mr-4 text-uppercase"
+      >
+        <span class="text-bar">User</span>
+      </v-btn>
 
-        <v-divider inset vertical></v-divider>
+      <v-btn flat @click="redirectToNewProduct()" color="none" class="mr-4 text-uppercase">
+        <p class="text-bar"><span class="blue">+ADD</span>PRODUCT</p>
+      </v-btn>
+      <v-divider class="mx-4" vertical inset />
 
-        <v-btn class="mr-4 ml-5" to="/login" color="none" flat @click="logout()">
-          <span class="text-bar">Logout</span>
-        </v-btn>
+      <v-btn class="mr-4 ml-5" to="/login" color="none" flat @click="logout()">
+        <span class="text-bar">Logout</span>
+      </v-btn>
+    </v-list>
+    <v-banner-content class="hidden-md-and-up">
+      <v-btn @click="showDrawer()" color="none" flat text>
+        <i class="fas fa-bars"></i>
+      </v-btn>
+    </v-banner-content>
+  </v-app-bar>
+  <v-navigation-drawer
+    v-model="drawer"
+    position="right"
+    width="1000"
+    elevation="0"
+    color="white"
+    border="none"
+    class="hidden-md-and-up"
+  >
+    <v-list nav dense align="center">
+      <div active-class="deep-purple--text text--accent-4">
+        <v-list-item
+          class="pl-7 text-bar"
+          title="Home"
+          @click="redirectToHome(), showDrawer()"
+        ></v-list-item>
+        <v-list-item
+          class="pl-7 text-bar"
+          title="User"
+          @click="redirectToUser(), showDrawer()"
+        ></v-list-item>
+        <v-list-item
+          class="pl-7 text-bar"
+          title="+ ADD PRODUCT"
+          @click="redirectToNewProduct(), showDrawer()"
+        ></v-list-item>
+        <v-spacer></v-spacer>
+        <v-list-item
+          class="pl-7 text-bar"
+          title="Logout"
+          @click="logout(), showDrawer()"
+        ></v-list-item>
       </div>
-      <div class="hidden-md-and-up">
-        <v-btn @click="showDrawer()" color="none" flat text>
-          <i class="fas fa-bars"></i>
-        </v-btn>
-      </div>
-    </v-app-bar>
-    <v-navigation-drawer
-      v-model="drawer"
-      position="right"
-      width="1000"
-      elevation="0"
-      color="white"
-      border="none"
-      class="hidden-md-and-up"
-    >
-      <v-list nav dense align="center">
-        <div active-class="deep-purple--text text--accent-4">
-          <v-list-item
-            class="pl-7 text-bar"
-            title="Home"
-            @click="redirectToHome(), showDrawer()"
-          ></v-list-item>
-          <v-list-item
-            class="pl-7 text-bar"
-            title="User"
-            @click="redirectToUser(), showDrawer()"
-          ></v-list-item>
-        </div>
-      </v-list>
-    </v-navigation-drawer>
-  </nav>
+    </v-list>
+  </v-navigation-drawer>
 </template>
 
 <script>
@@ -82,7 +94,7 @@ export default defineComponent({
 
   methods: {
     ...mapActions(["loginUser", "logoutUser"]),
-    ...mapGetters(["redirectToHome", "redirectToUser", "redirectToLogin"]),
+    ...mapGetters(["redirectToHome", "redirectToUser", "redirectToLogin", "redirectToNewProduct"]),
 
     logout() {
       this.logoutUser();
@@ -109,5 +121,18 @@ export default defineComponent({
 }
 .text-bar {
   font-weight: 500;
+}
+.blue {
+  color: #207eeb;
+}
+.world {
+  font-weight: 600;
+}
+.vertical-line {
+  width: 1px;
+  height: 30px;
+  background-color: rgb(80, 80, 80);
+  margin: 0;
+  padding: 0;
 }
 </style>
