a1866ebd5b41dcd54fc10b019b900d9a54d301ab
Claudia Font <claudiafontmarques@gmail.com>
2022-01-21 19:57:31 +0100

    created new product form component

---
+++ src/components/ProductForm/ProductForm.vue
@@ -0,0 +1,147 @@
+<template>
+  <v-row justify="center">
+    <v-col cols="10" sm="10" md="8" lg="6">
+      <v-card ref="onSubmit" class="pa-3 product-card">
+        <v-card-title class="product mt-12 mb-10">
+          <p class="product__title">
+            ADD YOUR PRODUCTS TO
+            <span class="world"> BLUE<span class="blue">WORLD</span></span>
+          </p>
+        </v-card-title>
+        <v-card-text>
+          <v-text-field
+            ref="name"
+            v-model="name"
+            :rules="[() => !!name || 'This field is required']"
+            label="Name"
+            required
+            color="#207eeb"
+            placeholder="Campbell's"
+          ></v-text-field>
+          <v-text-field
+            outlined
+            ref="price"
+            v-model="price"
+            :rules="[() => !!price || 'This field is required']"
+            label="Price"
+            placeholder="2.50"
+            type="number"
+            color="#207eeb"
+            required
+          ></v-text-field>
+          <v-textarea
+            ref="description"
+            rows="5"
+            row-height="30"
+            v-model="description"
+            maxlength="400"
+            :rules="[() => !!description || 'This field is required']"
+            label="Description"
+            counter="400"
+            color="#207eeb"
+            single-line
+            required
+            placeholder="Campbell's condensed is a tomato Soup "
+          ></v-textarea>
+        </v-card-text>
+        <v-card-actions>
+          <v-spacer></v-spacer>
+          <v-btn
+            variant="outlined"
+            class="mb-1 mr-2"
+            :disabled="name === '' || description === '' || price === ''"
+            color="#207eeb"
+            text
+            @click="onSubmit"
+          >
+            CREATE PRODUCT
+          </v-btn>
+        </v-card-actions>
+      </v-card>
+      <div class="errorMessage">
+        <v-alert v-if="wrongProduct" variant="outlined" density="compact" color="#207eeb">
+          ! This is a wrong Product
+        </v-alert>
+      </div>
+    </v-col>
+  </v-row>
+</template>
+
+<script>
+import { defineComponent } from "vue";
+import { mapActions, mapGetters, mapState } from "vuex";
+
+export default defineComponent({
+  name: "Login",
+  data() {
+    return {
+      name: "Pencil",
+      description: "Blue pencil",
+      price: 1.0,
+      isDisabled: true,
+      wrongProduct: false,
+    };
+  },
+  computed: {
+    ...mapState(["user"]),
+  },
+  methods: {
+    ...mapActions(["addProducts"]),
+    ...mapGetters(["redirectToHome"]),
+    async onSubmit() {
+      try {
+        this.wrongProduct = false;
+        if (this.name !== "" && this.description !== "" && this.price !== "") {
+          const { clientKey } = JSON.parse(
+            // eslint-disable-next-line comma-dangle
+            localStorage.getItem("clientKey") || ""
+          );
+          const productData = {
+            name: this.name,
+            description: this.description,
+            price: this.price,
+            clientKey,
+          };
+          const status = await this.addProducts(productData);
+          if (status === 200) {
+            this.redirectToHome();
+          } else {
+            this.wrongProduct = true;
+          }
+        }
+      } catch {
+        this.wrongProduct = true;
+      }
+    },
+  },
+});
+</script>
+
+<style lang="scss" scoped>
+.product {
+  text-align: center;
+  align-items: center;
+  justify-content: center;
+  &__title {
+    font-size: 26px;
+    font-family: "Roboto";
+    font-weight: 400;
+  }
+}
+.product-card {
+  box-shadow: 0 0 10px rgba(32, 126, 235, 0.2);
+}
+.errorMessage {
+  margin-top: 30px;
+  height: 60px;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+}
+.blue {
+  color: #207eeb;
+}
+.world {
+  font-weight: 600;
+}
+</style>
