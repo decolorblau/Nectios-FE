633102352c591bb490efdc52b4073e99e5616854
Claudia Font <claudiafontmarques@gmail.com>
2022-01-23 02:35:43 +0100

    fixed internal bug calling function on parent child

--- src/components/ProductCard/ProductCard.vue
+++ src/components/ProductCard/ProductCard.vue
@@ -1,21 +1,20 @@
 <template>
-  <v-card class="ma-1 mb-2" width="269.5" @click="redirectToDetail()">
+  <v-card class="ma-1 mb-2 pb-1" width="269.5" @click="redirectToDetail()">
     <v-img
       :src="
         product.product_photo ===
         'https://boltagency.ca/content/images/2020/03/placeholder-images-product-1_large.png'
           ? product.product_photo
-          : 'https://images.unsplash.com/photo-1641831705160-5d56ac4094cb?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=580&q=80'
+          : 'https://images.theconversation.com/files/387012/original/file-20210301-12-pyj8jo.jpg?ixlib=rb-1.1.0&q=45&auto=format&w=1200&h=1200.0&fit=crop'
       "
       height="250px"
       class="mt-3 rounded"
       width="250px"
       lazy-src="https://c.tenor.com/-NoKc-auITEAAAAC/loading-buffering.gif"
     ></v-img>
-    <div class="">
+    <div>
       <v-card-title class="pb-1"> {{ product.name.toUpperCase() }} </v-card-title>
-      <v-spacer></v-spacer>
-      <v-card-subtitle class="pb-2">{{ product.price }} €</v-card-subtitle>
+      <v-card-subtitle class="pb-2 price">{{ product.price }} €</v-card-subtitle>
     </div>
   </v-card>
 </template>
@@ -29,14 +28,33 @@ export default {
   data() {
     return {
       pathProduct: `/products/${this.product.id}`,
+      currentProduct: {
+        id: this.product.id,
+        key: this.product.key,
+        name: this.product.name,
+        description: this.product.description,
+        price: this.product.price,
+        photo: this.product.product_photo,
+        clientId: this.product.client_id,
+      },
+      id: this.product.id,
     };
   },
   methods: {
     redirectToDetail() {
       router.push(this.pathProduct);
+      const { currentProduct } = this;
+      localStorage.setItem("currentProduct", JSON.stringify({ currentProduct }));
     },
   },
 };
 </script>
 
-<style lang="scss" scoped></style>
+<style lang="scss" scoped>
+.price {
+  font-family: "Rubik";
+  color: #207eeb;
+  font-weight: 500;
+  font-size: 20px;
+}
+</style>
