f5a3b1e6ecd8d6678a18a9782ceaf31923d03a53
Claudia Font <claudiafontmarques@gmail.com>
2022-01-24 12:27:30 +0100

    added pagination tags buttons and functions

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -4,7 +4,7 @@
       <v-row fill-height no-gutters wrap style="height: 150px" class="table">
         <v-col
           d-flex
-          v-for="(product, i) in products"
+          v-for="(product, i) in pageProducts"
           :key="i"
           cols="12"
           sm="6"
@@ -14,6 +14,46 @@
           align-self="center"
         >
           <ProductCard :product="product" />
+        </v-col>
+        <v-col cols="12" class="pagination">
+          <v-btn
+            :disabled="counter === 0"
+            @click="firstPage()"
+            color="rgb(250, 250, 250)"
+            flat
+            class="hidden-sm-and-down pagination__button"
+            >&#60;&#60; First</v-btn
+          >
+          <v-btn
+            :disabled="counter === 0"
+            @click="beforePage()"
+            color="rgb(250, 250, 250)"
+            class="pagination__button"
+            flat
+          >
+            &#60; Before</v-btn
+          >
+          <p class="pagination__text">
+            page {{ counter + 1 }} of {{ Math.ceil(products.length / elementsInPage) }}
+          </p>
+          <v-btn
+            :disabled="(counter + 1) * elementsInPage >= products.length"
+            @click="nextPage()"
+            color="rgb(250, 250, 250)"
+            class="pagination__button"
+            flat
+          >
+            Next &#62;</v-btn
+          >
+          <v-btn
+            :disabled="(counter + 1) * elementsInPage >= products.length"
+            @click="lastPage()"
+            color="rgb(250, 250, 250)"
+            flat
+            class="hidden-sm-and-down pagination__button"
+          >
+            Last &#62;&#62;</v-btn
+          >
         </v-col>
         <v-divider class="mt-6"></v-divider>
         <v-col cols="12" class="footer">
@@ -32,27 +72,57 @@ import ProductCard from "../components/ProductCard/ProductCard.vue";
 
 export default defineComponent({
   name: "Home",
+  data() {
+    return {
+      page: this.counter + 1,
+      counter: 0,
+      elementsInPage: 12,
+    };
+  },
+
   components: {
     ProductCard,
     Footer,
   },
 
   computed: {
-    ...mapState(["user", "products"]),
+    ...mapState(["user", "products", "pageProducts"]),
   },
   methods: {
-    ...mapActions(["checkToken", "getProducts"]),
+    ...mapActions([
+      "checkToken",
+      "getProducts",
+      "getPageProducts",
+      "counterDecrease",
+      "counterIncrease",
+    ]),
     ...mapGetters(["redirectToLogin"]),
-  },
 
-  mounted() {
-    this.getProducts();
+    nextPage() {
+      this.counter++;
+      this.getProducts(this.counter);
+    },
+    beforePage() {
+      this.counter--;
+      this.getProducts(this.counter);
+    },
+    firstPage() {
+      this.counter = 0;
+      this.getProducts(this.counter);
+    },
+    lastPage() {
+      this.counter = Math.floor(this.products.length / this.elementsInPage);
+      this.getProducts(this.counter);
+    },
   },
-  beforeMount() {
+  created() {
     this.checkToken();
     if (!this.user.isAuthenticated) {
       this.redirectToLogin();
     }
+  },
+  mounted() {
+    this.getProducts(this.counter);
   },
 });
 </script>
@@ -69,10 +139,25 @@ export default defineComponent({
   min-height: 100vh;
 }
 .footer {
+  align-items: center;
+  justify-content: center;
   position: relative;
   bottom: 0;
 }
 .table {
   background-color: rgb(250, 250, 250);
+}
+.pagination {
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  margin-top: 50px;
+  color: #999999;
+  &__button {
+    margin: 0 10px;
+  }
+  &__text {
+    margin: 0 30px;
+  }
 }
 </style>
